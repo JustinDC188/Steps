960403ae28206beeae17c88ca61a0c8a55f91ad3
Justin Cius <justin.cius68@gmail.com>
2025-11-03 10:42:12 -0500

    Version 1

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛",
+  "Apply for jobs 🏢",
+  "Invest in your Income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  function previous() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function next() {
+    if (step < 3) setStep(step + 1);
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={previous}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={next}
+          // onMouseEnter={}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
